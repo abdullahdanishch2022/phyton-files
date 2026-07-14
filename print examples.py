c406ea4154aff4a996597179056718f64ffc9b48
abdullahdanishch2022 <abdullahdanishch2022@gmail.com>
2026-07-14 17:25:16 -0400

    print examples

---
+++ print examples.py
@@ -0,0 +1,25 @@
+#print all the keywords in phython
+import keyword
+
+print("phyton kewords are: \n")
+print(keyword.kwlist)
+
+#print examples
+
+print("my name is abdullah")
+print(5)
+print("im in 7th grade \n")
+print("l",5)
+print("welcome to codingal",end="+")
+
+#another print , variable example
+
+x=4
+y="apple"
+print(x)
+print(y)
+str="codingal"
+print(str)
+name=input("enter your name")
+print("\n hello ",name,"\n welcome to codingal")
+
