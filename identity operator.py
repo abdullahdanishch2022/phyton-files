02d85281735d21ae10fed77363cb73202fe1b0bc
abdullahdanishch2022 <abdullahdanishch2022@gmail.com>
2026-07-24 17:49:37 -0400

    identity operator

---
+++ identity operator.py
@@ -0,0 +1,20 @@
+x=[1,2,3,4,5]
+y=[1,2,3,4,5]
+z=x
+print(x is y)
+print(x is not y)
+print(x is z)
+print(z is x)
+print(z is not x)
+print(x is not z)
+a=10
+b=5.5
+if(type(a) is int):
+    print("true")
+else:
+    print("false")
+if(type(b) is float):
+    print("true")
+else:
+    print("false")
+                
