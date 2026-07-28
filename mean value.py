36462850247b4cfb12dbb7f6c893e89ae6ee8ae5
abdullahdanishch2022 <abdullahdanishch2022@gmail.com>
2026-07-28 17:30:39 -0400

    mean value

---
+++ mean value.py
@@ -0,0 +1,9 @@
+mean1=38
+totalnumber=40
+wrong_number=36
+correct_number=56
+sum=mean1*totalnumber
+num2=sum-wrong_number+correct_number
+mean2=num2/totalnumber
+print("original mean was", mean1)
+print("correct mean is", mean2)
