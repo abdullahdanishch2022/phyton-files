eb63d2487ada159574b995f580805c2fa3ee1383
abdullahdanishch2022 <abdullahdanishch2022@gmail.com>
2026-07-28 17:10:26 -0400

    operator precedence

---
+++ operator precedence.py
@@ -0,0 +1,13 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v + w)*x/y
+print("the result of expression is", z)
+name="alex"
+age=1
+if (name=="alex" or name=="john") and age>=2:
+    print("welcome alex")
+else:
+    print("bye bye")
