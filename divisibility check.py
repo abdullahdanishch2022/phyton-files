a3f8cef0852b98021769993444165322c64d71fd
abdullahdanishch2022 <abdullahdanishch2022@gmail.com>
2026-07-28 17:23:47 -0400

    divisibility check

---
+++ divisibility check.py
@@ -0,0 +1,7 @@
+numn=int(input("enter the numerator"))
+numd=int(input("enter the denominator"))
+
+if numn%numd==0:
+    print(f"{numn} is divisible by {numd}")
+else:
+    print(f"{numn} is not divisible by {numd}")
